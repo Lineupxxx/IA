cde7cd725bd63a713c3c13a2baab705eeed1c202
L1nus <160563634+Lineupxxx@users.noreply.github.com>
2024-10-01 22:47:02 -0600

    Add files via upload

---
+++ IA/Inicio/practica1.py
@@ -0,0 +1,15 @@
+# Imprimir "Hola, como estan" en Python
+print("Hola, como estan")
+
+# Operaciones matemáticas básicas
+a = 10
+b = 20
+c = 30
+suma = a + b
+print(f"La suma de {a} y {b} es: {suma}")
+resta = c - b - a
+print(f"La resta de {c} , {b} y {a} es: {resta}")
+mult = a * b * c
+print(f"La multiplicacion de {a} , {b} y {c} es: {mult}")
+division = (c / b) / a
+print(f"La division de {c} , {b} y {a} es: {division}")
